4e56f1c2967e41bdccd7853dd219d54d4d3b5af9
Weslley Leocadio <silvawesley374@gmail.com>
2019-07-19 10:41:02 -0300

    REST TESTADO

---
+++ src/main/java/com/wesley/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,14 @@
+package com.wesley.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando";
+	}
+}
