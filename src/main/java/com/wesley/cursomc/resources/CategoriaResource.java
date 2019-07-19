12476ed9704f4b1476c977fb140e801bc78b2a8c
Weslley Leocadio <silvawesley374@gmail.com>
2019-07-19 11:22:19 -0300

    TESTADO A PRIMEIRA CLASSE DE DOMINIO

--- src/main/java/com/wesley/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/wesley/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,24 @@
 package com.wesley.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wesley.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1= new Categoria(1,"informática");
+		Categoria cat2 =  new Categoria(2,"Escritório");
+		List<Categoria> lista =  new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
