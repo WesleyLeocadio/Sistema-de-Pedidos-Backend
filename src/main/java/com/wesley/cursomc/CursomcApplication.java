20288209df2695d4fbe52eb3c0f59320b48f8e45
Weslley Leocadio <silvawesley374@gmail.com>
2019-07-24 21:13:43 -0300

    CRIANDO OPERAÇÃO DE INTANCIAÇÃO

--- src/main/java/com/wesley/cursomc/CursomcApplication.java
+++ src/main/java/com/wesley/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.wesley.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wesley.cursomc.domain.Categoria;
+import com.wesley.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 
 }
--- src/main/java/com/wesley/cursomc/services/CategoriaService.java
+++ src/main/java/com/wesley/cursomc/services/CategoriaService.java
@@ -15,5 +15,6 @@ public class CategoriaService {
 		Categoria obj = repo.findOne(id);
 		return obj;
 	}
+	
 
 }
