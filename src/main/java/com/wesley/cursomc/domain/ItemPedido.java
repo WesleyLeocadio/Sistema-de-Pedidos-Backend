07d2741feaf2e1d421dd3f374d7ee3a65ae405d4
WesleyLeocadio <silvawesley374@gmail.com>
2020-05-20 10:26:04 -0300

    Subtotal e total do Pedido

--- src/main/java/com/wesley/cursomc/domain/ItemPedido.java
+++ src/main/java/com/wesley/cursomc/domain/ItemPedido.java
@@ -71,6 +71,10 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
