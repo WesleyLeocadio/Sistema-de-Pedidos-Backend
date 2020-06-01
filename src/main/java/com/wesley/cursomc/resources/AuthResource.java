12d6a11f7fa76d4b2a717798d8eef62170e2a0f1
WesleyLeocadio <silvawesley374@gmail.com>
2020-06-01 08:49:09 -0300

    Refresh token

---
+++ src/main/java/com/wesley/cursomc/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.wesley.cursomc.resources;
+
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.wesley.cursomc.security.JWTUtil;
+import com.wesley.cursomc.security.UserSS;
+import com.wesley.cursomc.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+}
